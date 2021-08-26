bf205a17a51f5b4d97e539448b3d9b015a2549c2
3st4r0s4 <daniele.dibattista@protonmail.com>
2021-08-27 00:07:05 +0200

    Second Web Challenge

--- pages/challenges/Web-challenges/[id].js
+++ pages/challenges/Web-challenges/[id].js
@@ -24,7 +24,7 @@ export default function WebDetails({ wch }) {
           <ReactPlayer
             width={"100%"}
             height={"auto"}
-            light={wch.coverUrl}
+            light={wch.coverUrl.formats.large.url}
             url={wch.video[0].url}
             controls
           />
