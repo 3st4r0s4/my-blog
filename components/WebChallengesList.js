4d1f631a302102a47b09b90724afc21da03dbd1d
3st4r0s4 <daniele.dibattista@protonmail.com>
2021-08-29 22:24:06 +0200

    Web Challenge 03

--- components/WebChallengesList.js
+++ components/WebChallengesList.js
@@ -3,6 +3,7 @@ import WebChPreview from "./WebChPreview";
 
 export default function WebChallengesList({ wchs }) {
   function renderWchPreviews() {
+    wchs.sort((a, b) => b.id - a.id);
     return wchs.map((wch) => {
       return <WebChPreview wch={wch} key={wch.id} />;
     });
