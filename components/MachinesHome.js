546a3d92460e4b8bd756ae4daa78ac20e9d77887
3st4r0s4 <daniele.dibattista@protonmail.com>
2021-09-07 17:45:06 +0200

    UI UPDATE

--- components/MachinesHome.js
+++ components/MachinesHome.js
@@ -9,7 +9,7 @@ export default function MachinesHome({ machines }) {
     machines.sort((a, b) => b.id - a.id);
     return machines.map((machine) => {
       return (
-        <Grid key={machine.id} item xs={12} sm={8} md={3} lg={4}>
+        <Grid key={machine.id} item xs={12} sm={6} md={3} lg={4}>
           <MachineCard machine={machine} />
         </Grid>
       );
@@ -18,7 +18,7 @@ export default function MachinesHome({ machines }) {
 
   return (
     <>
-      <Grid container alignItems="center" justify="center" spacing={2}>
+      <Grid container alignItems="center" justify="center" spacing={4}>
         {renderMachinesCards()}
       </Grid>
     </>
