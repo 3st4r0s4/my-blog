9c224b1911252fb9169753ac0451e78818c3ba79
Daniele Di Battista <daniele.dibattista@protonmail.com>
2021-11-04 11:52:13 +0100

    Under Construction

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <NavBar />
 
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
 
       <img src="uc.jpg"></img>
 
